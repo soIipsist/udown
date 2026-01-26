552801fae4bf3a70c7c7341babbaa87984a09324
soIipsist <solipsist14@gmail.com>
2026-01-26 15:58:09 +0200

    tui options - modify field values

--- src/tui_options.py
+++ src/tui_options.py
@@ -1,5 +1,33 @@
 from textual.widgets import DataTable
 from textual import events
+from textual.screen import ModalScreen
+from textual.widgets import Input, Button
+from textual.containers import Vertical
+from textual.coordinate import Coordinate
+
+
+class EditOption(ModalScreen):
+
+    def __init__(self, key: str, value: str):
+        super().__init__()
+        self.key = key
+        self.value = value
+
+    def compose(self):
+        yield Vertical(
+            Input(value=str(self.value), id="value_input"),
+            Button("Save", id="save", variant="primary"),
+            Button("Cancel", id="cancel"),
+        )
+
+    def on_button_pressed(self, event: Button.Pressed):
+        if event.button.id == "save":
+            value = self.query_one(Input).value
+            from src.options import set_option
+
+            set_option(self.key, value)
+
+        self.dismiss()
 
 
 class OptionsTable(DataTable):
@@ -46,4 +74,7 @@ class OptionsTable(DataTable):
         row = self.cursor_row
         if row is None:
             return
-        self.notify(str(row))
+
+        key = str(self.get_cell_at(Coordinate(row, 0)))
+        value = str(self.get_cell_at(Coordinate(row, 1)))
+        self.app.push_screen(EditOption(key, value))
