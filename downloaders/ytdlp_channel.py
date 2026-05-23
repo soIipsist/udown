264cc78f0da54f40679834a9c4f3b8d9e8cb9521
soIipsist <solipsist14@gmail.com>
2026-05-23 20:53:18 +0300

    ytdlp_channel

--- downloaders/ytdlp_channel.py
+++ downloaders/ytdlp_channel.py
@@ -12,22 +12,11 @@ downloader_types = get_downloader_types()
 
 def download(
     channel_id: str,
-    downloader: str = None,
+    downloader_type: str = get_setting("DOWNLOADER_TYPE", "ytdlp_video"),
     proxy: str = None,
     sleep_interval: str = "2",
     max_sleep_interval: str = "5",
 ):
-
-    if downloader is None:
-        downloader = get_setting("DOWNLOADER_TYPE", "ytdlp_video")
-        downloader = Downloader(downloader)
-
-    downloader = Downloader(downloader_type=downloader).select_first()
-
-    if not downloader:
-        raise ValueError(f"Downloader of type {downloader} was not found.")
-
-    downloader: Downloader
 
     results = get_channel_info(channel_id)
     video_urls = [
@@ -37,7 +26,7 @@ def download(
     downloads = [
         Download(
             url=video_url,
-            downloader_type=downloader,
+            downloader_type=downloader_type,
             output_directory=os.environ.get("DOWNLOAD_DIRECTORY"),
             proxy=proxy,
             extra_args={
@@ -48,7 +37,7 @@ def download(
         for video_url in video_urls
     ]
 
-    return downloader.start_downloads(downloads)
+    return downloader_type.start_downloads(downloads)
 
 
 if __name__ == "__main__":
@@ -56,7 +45,7 @@ if __name__ == "__main__":
     parser.add_argument("channel_id", type=str)
     parser.add_argument(
         "-d",
-        "--downloader",
+        "--downloader_type",
         default=get_setting("DOWNLOADER_TYPE", "ytdlp_video"),
         choices=downloader_types,
     )
@@ -66,7 +55,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
     download(
         args.channel_id,
-        args.downloader,
+        args.downloader_type,
         args.proxy,
         args.sleep_interval,
         args.max_sleep_interval,
